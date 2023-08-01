80ed3e5adde73ad3d09cccb9233f661f5062dd9c
YuraBobeshko <gorobech55@gmail.com>
2023-08-01 22:43:52 +0200

    init

---
+++ SentencesParserTask.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var symbolsToCheck = ".!?;:()";
+            var shouldAddNewList = true;
+            var shouldAddToExistedWord = false;
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    if (shouldAddNewList)
+                    {
+                        sentencesList.Add(new List<string>(text[i]));
+                    } 
+                    else if (shouldAddToExistedWord)
+                    {
+                        var index = sentencesList.Count - 1;
+                        if (sentencesList[index].Count == 0)
+                        {
+                            sentencesList[index][sentencesList[index].Count] = text[i].ToString();
+                        } 
+                        else
+                        {
+                            sentencesList[index][sentencesList[index].Count - 1] += text[i].ToString();
+                        }
+                    } 
+                    else
+                    {
+                        sentencesList[sentencesList.Count - 1].Add(text[i].ToString());
+                    }
+
+                    shouldAddNewList = false;
+                    shouldAddToExistedWord = true;
+                } 
+                else if (symbolsToCheck.Contains(text[i].ToString()) )
+                {
+                    shouldAddNewList = true;
+                }  
+                else
+                {
+                    shouldAddToExistedWord = false;
+                }
+            }
+            
+            return sentencesList;
+        }
+    }
+}
